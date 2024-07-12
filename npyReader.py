6b8b9375b375bdc560839be0eaf4722078222fd7
Yash Tiwari <yashvtiwari04@gmail.com>
2024-07-12 14:07:40 -0500

    Documentation finished. testData has an example of structured and unstructured .npy files. readMetadataNPY.m has been updated to readNPYFile.m to better represent its capability of a general .npy data loader.

--- npyReader.py
+++ npyReader.py
@@ -4,30 +4,31 @@ import sys
 import os
 import re
 
-# file to read in 
+# File to read in 
 f = sys.argv[1]
 
-#f = 'r"' + f + '"'
-
-# file name to output
+# File name to output
 fNameCSV = os.path.splitext(f)[0]+'.csv'
-data = np.load(f)
+data = np.load(f) # load data 
 
+# Check if data can be read normally. 
+# This is to determine if the file is structured or not.
+# This will work for an unstructured file.  
 try:
 
-    # check if data can be read normally 
     DF = pd.DataFrame(data)
     DF.to_csv(fNameCSV)
 
+# This is the reader for structured array .npy files. 
 except:
 
-    # write header directly to csv file
+    # Write header directly to csv file.
     header = np.lib.format.header_data_from_array_1_0(data) # get header
     out = open(fNameCSV, "w")
-    # parse fields from header
+    # Parse fields from header.
     header = re.sub("\s", "_", str(header))
     fields = re.findall("'([A-Z]\w*)+'*", header)
-    # write fields to csv 
+    # Write fields to csv. 
     i = 0
     for field in fields:
         out.write(str(field))
@@ -36,7 +37,7 @@ except:
                 out.write(',')
     out.write('\n')
 
-    # wirte data to csv file
+    # Wirte data to csv file.
     for tup in data:
         i = 0
         for item in tup:
@@ -46,5 +47,6 @@ except:
                 out.write(',')
         out.write('\n')
 
+# Prints out location of the .csv file, it should be in the same directory as the original .npy file.
 finally: 
     print('generated ', fNameCSV, 'from', f)
--- readNPYFile.m
+++ readNPYFile.m
@@ -1,11 +1,15 @@
 function data = readNPYFile(filename)
 
-npyFilePath = append("'", filename, "'");
-pyrunfile(append("npyReader.py ", npyFilePath));
+% Encapsulates filename with "'" to support paths with whitespace. 
+npyFilePath = append("'", filename, "'");  
+% Call python script with correct file path.
+pyrunfile(append("npyReader.py ", npyFilePath)); 
 
+% Reads temp .csv file and constructs a struct.
 csvFilePath = append(filename(1:end-3), "csv");
 T = readtable(csvFilePath, 'Decimal','.', 'Delimiter',',');
 S = table2struct(T);
 data = S;
 
+% deletes temp .csv file.
 delete(csvFilePath);
