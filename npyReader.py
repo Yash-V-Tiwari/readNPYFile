4134180b9743c0dde3b20c022ba42ff8dd64f729
Yash Tiwari <yashvtiwari04@gmail.com>
2024-07-12 11:58:55 -0500

    Finished metadata loader. Loader can now read a file in from specified folder, generate a csv, make a struct, then delete the csv. Supports having filepaths with whitespace

--- npyReader.py
+++ npyReader.py
@@ -2,6 +2,7 @@ import numpy  as np
 import pandas as pd
 import sys
 import os
+import re
 
 # file to read in 
 f = sys.argv[1]
@@ -14,17 +15,28 @@ data = np.load(f)
 
 try:
 
-    #check if data can be read normally 
+    # check if data can be read normally 
     DF = pd.DataFrame(data)
     DF.to_csv(fNameCSV)
 
 except:
 
-    #write to csv file directly with header
+    # write header directly to csv file
+    header = np.lib.format.header_data_from_array_1_0(data) # get header
     out = open(fNameCSV, "w")
-    out.write('Crossing_Point,Crossing_level,Threshold,Direction,Learning_rate') #update this to be dynamic
+    # parse fields from header
+    header = re.sub("\s", "_", str(header))
+    fields = re.findall("'([A-Z]\w*)+'*", header)
+    # write fields to csv 
+    i = 0
+    for field in fields:
+        out.write(str(field))
+        i += 1
+        if (i != len(fields)):
+                out.write(',')
     out.write('\n')
 
+    # wirte data to csv file
     for tup in data:
         i = 0
         for item in tup:
--- readMetadataNPY.m
+++ readMetadataNPY.m
@@ -1,14 +1,11 @@
 function data = readMetadataNPY(filename)
 
-pyrunfile(append("npyReader.py ", filename));
+npyFilePath = append("'", filename, "'");
+pyrunfile(append("npyReader.py ", npyFilePath));
 
-[file,path] = uigetfile();
-if isequal(file,0)
-   disp('User selected Cancel');
-else
-   %csvFilePath = append(filename(1:end-3), "csv");
-   T = readtable(fullfile(path,file), 'Decimal','.', 'Delimiter',',');
-   S = struct('Crossing_Point',T.Crossing_Point, 'Crossing_level',T.Crossing_level, ...
-           'Threshold',T.Threshold, 'Direction',T.Direction, 'Learning_rate',T.Learning_rate);
-   data = S;
-end
+csvFilePath = append(filename(1:end-3), "csv");
+T = readtable(csvFilePath, 'Decimal','.', 'Delimiter',',');
+S = table2struct(T);
+data = S;
+
+delete(csvFilePath);
